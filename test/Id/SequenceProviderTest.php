08d5fc01585616cb231df5891dd3140e2612b49c
Hendrik Legge <admin@themepoint.de>
2022-10-16 13:22:30 +0200

    Added tests for SequenceProvider

---
+++ test/Id/SequenceProviderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Flexic\DataProvider\Test\Id;
+
+use Flexic\DataProvider\Test\AbstractTestCase;
+use Flexic\DataProvider\Test\TestUtil;
+
+/**
+ * @internal
+ *
+ * @covers \Flexic\DataProvider\Id\SequenceProvider
+ */
+class SequenceProviderTest extends AbstractTestCase
+{
+    public function testShortSequence(): void
+    {
+        $value = TestUtil::array(\Flexic\DataProvider\Id\SequenceProvider::short());
+
+        self::assertCount(10, $value);
+        self::assertLessThanOrEqual(10, \count($value));
+        self::assertArrayHasKeyBetween(0, 9, $value);
+    }
+
+    public function testLongSequence(): void
+    {
+        $value = TestUtil::array(\Flexic\DataProvider\Id\SequenceProvider::long());
+
+        self::assertCount(1000, $value);
+        self::assertLessThanOrEqual(1000, \count($value));
+        self::assertArrayHasKeyBetween(0, 999, $value);
+    }
+
+    public function testExtraLongSequence(): void
+    {
+        $value = TestUtil::array(\Flexic\DataProvider\Id\SequenceProvider::extraLong());
+
+        self::assertCount(100000, $value);
+        self::assertLessThanOrEqual(100000, \count($value));
+        self::assertArrayHasKeyBetween(0, 99999, $value);
+    }
+}
