9429c619f90b1a3c2168a1a1fe1b4680cea5b168
Hendrik Legge <admin@themepoint.de>
2022-10-30 23:46:51 +0100

    Run cs fixer

--- test/Code/HtmlProviderTest.php
+++ test/Code/HtmlProviderTest.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\DataProvider\Test\Code;
 
 use Flexic\DataProvider\Test\AbstractTestCase;
@@ -10,7 +20,7 @@ use Flexic\DataProvider\Test\TestUtil;
  *
  * @covers \Flexic\DataProvider\Code\HtmlProvider
  */
-class HtmlProviderTest extends AbstractTestCase
+final class HtmlProviderTest extends AbstractTestCase
 {
     public function testArbitrary(): void
     {
