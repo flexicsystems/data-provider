f294dd7cbc034480dcfed51bb809f3ef629e5cc7
Hendrik Legge <admin@themepoint.de>
2022-10-31 15:02:08 +0100

    Updated provider

--- src/Miscellaneous/BarcodeProvider.php
+++ src/Miscellaneous/BarcodeProvider.php
@@ -19,6 +19,14 @@ final class BarcodeProvider extends AbstractProvider
     /**
      * @return \Generator<string, array{0: string}>
      */
+    public static function arbitrary(): \Generator
+    {
+        yield from self::provideDataForValues(self::values());
+    }
+
+    /**
+     * @return \Generator<string, array{0: string}>
+     */
     public static function ean13(): \Generator
     {
         yield from self::provideDataForValuesWhereKey(self::values(), static function (string $key): bool {
