e1fda5fe551594d23e7a254e11420eff3053b0d6
Hendrik Legge <admin@themepoint.de>
2022-10-31 21:30:03 +0100

    Run cs fixer

--- src/Payment/CreditCardDetailsProvider.php
+++ src/Payment/CreditCardDetailsProvider.php
@@ -1,10 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\DataProvider\Payment;
 
 use Flexic\DataProvider\AbstractProvider;
 
-class CreditCardDetailsProvider extends AbstractProvider
+final class CreditCardDetailsProvider extends AbstractProvider
 {
     /**
      * @return \Generator<string, array{0: array}>
--- test/Payment/CreditCardDetailsProviderTest.php
+++ test/Payment/CreditCardDetailsProviderTest.php
@@ -1,16 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\DataProvider\Test\Payment;
 
 use Flexic\DataProvider\Test\AbstractTestCase;
-use Flexic\DataProvider\Test\TestUtil;
 
 /**
  * @internal
  *
  * @covers \Flexic\DataProvider\Payment\CreditCardDetailsProvider
  */
-class CreditCardDetailsProviderTest extends AbstractTestCase
+final class CreditCardDetailsProviderTest extends AbstractTestCase
 {
     public function testArbitrary(): void
     {
