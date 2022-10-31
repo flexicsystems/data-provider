c30219ef9f31e8468d9e27e0cd4372c6e36302d6
Hendrik Legge <admin@themepoint.de>
2022-10-31 21:09:18 +0100

    Added test

---
+++ test/Payment/CreditCardExpirationProviderTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
+namespace Flexic\DataProvider\Test\Payment;
+
+use Flexic\DataProvider\Test\AbstractTestCase;
+use Flexic\DataProvider\Test\TestUtil;
+
+/**
+ * @internal
+ *
+ * @covers \Flexic\DataProvider\Payment\CreditCardExpirationProvider
+ */
+final class CreditCardExpirationProviderTest extends AbstractTestCase
+{
+    public function testArbitrary(): void
+    {
+        $value = TestUtil::string(\Flexic\DataProvider\Payment\CreditCardExpirationProvider::arbitrary());
+
+        self::assertIsString($value);
+        self::assertMatchesRegularExpression('/^([0-9]{2}\/[0-9]{2})$/', $value);
+    }
+
+    public function testValid(): void
+    {
+        $value = TestUtil::string(\Flexic\DataProvider\Payment\CreditCardExpirationProvider::valid());
+
+        self::assertIsString($value);
+        self::assertMatchesRegularExpression('/^([0-9]{2}\/[0-9]{2})$/', $value);
+
+        $parts = \explode('/', $value);
+        $actual = new \DateTimeImmutable('now');
+
+        self::assertGreaterThanOrEqual((int) $actual->format('m'), (int) $parts[0]);
+        self::assertGreaterThanOrEqual((int) $actual->format('y'), (int) $parts[1]);
+    }
+
+    public function testExpired(): void
+    {
+        $value = TestUtil::string(\Flexic\DataProvider\Payment\CreditCardExpirationProvider::expired());
+
+        self::assertIsString($value);
+        self::assertMatchesRegularExpression('/^([0-9]{2}\/[0-9]{2})$/', $value);
+
+        $parts = \explode('/', $value);
+        $actual = new \DateTimeImmutable('now');
+
+        self::assertLessThan((int) $actual->format('m'), (int) $parts[0]);
+        self::assertLessThan((int) $actual->format('y'), (int) $parts[1]);
+    }
+}
