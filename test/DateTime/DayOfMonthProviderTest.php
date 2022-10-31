b0a8b294867ab5635a433bd2135aed5863259038
Hendrik Legge <admin@themepoint.de>
2022-10-31 19:49:11 +0100

    Run cs fixer

--- test/DateTime/DayOfMonthProviderTest.php
+++ test/DateTime/DayOfMonthProviderTest.php
@@ -1,11 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\DataProvider\Test\DateTime;
 
 use Flexic\DataProvider\Test\AbstractTestCase;
 use Flexic\DataProvider\Test\TestUtil;
 
-class DayOfMonthProviderTest extends AbstractTestCase
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class DayOfMonthProviderTest extends AbstractTestCase
 {
     public function testArbitrary(): void
     {
