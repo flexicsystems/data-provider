81836c740d6dfe84eae53b517a30988dec666d91
Hendrik Legge <admin@themepoint.de>
2022-11-01 15:07:57 +0100

    Run cs fixer

--- test/DateTime/CenturyProviderTest.php
+++ test/DateTime/CenturyProviderTest.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\DataProvider\Test\DateTime;
 
 use Flexic\DataProvider\Test\AbstractTestCase;
@@ -10,7 +20,7 @@ use Flexic\DataProvider\Test\TestUtil;
  *
  * @covers \Flexic\DataProvider\DateTime\CenturyProvider
  */
-class CenturyProviderTest extends AbstractTestCase
+final class CenturyProviderTest extends AbstractTestCase
 {
     public function testArbitrary(): void
     {
